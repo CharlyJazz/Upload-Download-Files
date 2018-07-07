7323f260c7aeed68d5278f7bc6b290fa334e46ed
Carlos Azuaje <carlosjazzc1@gmail.com>
2018-07-07 16:03:34 -0400

    refactor(update): webpack and react
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
 var path = require('path');
 
 const ENTRY_DIR = path.resolve(__dirname, 'app/static/javascript/src');
+const OUTPUT_DIR = path.resolve(__dirname, 'app/static/javascript/bin');
 
-var config = {
-	entry: ENTRY_DIR  + '/index.js',
+module.exports = {
+	entry: ENTRY_DIR + '/index.js',
 	output: {
-		path: path.resolve(__dirname, 'app/static/javascript/bin'),
+		path: OUTPUT_DIR,
 		filename: 'bundle.js'
 	},
-	module : {
-		loaders : [
+	module: {
+		rules: [
 			{
-				test : /\.js?/,
-				include : ENTRY_DIR,
-				loader : 'babel-loader'
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
 			},
 			{
 				test: /\.css$/,
-				use: [ 'style-loader', 'css-loader' ]
+				use: ['style-loader', 'css-loader']
 			}
 		]
 	}
 };
-
-module.exports = config;
